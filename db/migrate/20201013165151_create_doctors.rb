92c95c2792f957d5bbcda6db8477b16f6692895d
Tomas Perez <tomasitopg6@gmail.com>
2020-10-13 16:52:07 +0000

    Automatically backed up by Learn

--- db/migrate/20201013165151_create_doctors.rb
+++ db/migrate/20201013165151_create_doctors.rb
@@ -3,6 +3,8 @@ class CreateDoctors < ActiveRecord::Migration[5.0]
     create_table :doctors do |t|
       t.string :name
       t.string :department
+
+      t.timestamps null: false
     end
   end
 end
